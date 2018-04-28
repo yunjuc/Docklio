c599c89297c333f72ad626b78afe42ea4c263a7f
Yunju <yunju@Yunjus-MacBook-Air.local>
2018-04-28 13:43:58 -0700

    Add comments

--- app.py
+++ app.py
@@ -12,11 +12,19 @@ app = Flask(__name__)
 @app.route('/sms', methods=['POST'])
 def sms():
     '''Receive message and respond'''
+    # get sms message body
     body = request.form['Body']
+
+    # create response
     resp = MessagingResponse()
 
+    # check command
     if body == 'docker deploy' or body == 'Docker deploy':
+
+        # execute deploymenet script
         status = os.system('./run')
+
+        # confirm success for fail
         if status == 0:
             resp.message('Woohoo! Your app has been deployed.')
         else:
@@ -24,6 +32,7 @@ def sms():
     else:
         resp.message('Sorry, we cannot recognize the command.')
 
+    # return response
     return str(resp)
 
 
